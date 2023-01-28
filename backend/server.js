50f2d17531a1090cd60b21d7a59914a75156588c
dev_Ayush <ayushpurohit140@gmail.com>
2023-01-28 11:28:08 +0530

    did deployment setup

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config()
 const app = express();
@@ -16,13 +17,30 @@ connectDB();
 
 app.use(express.json());//to accept json data from frontend
 
-app.get("/", (req,res)=>{
-    res.send("API is running");
-})
+// app.get("/", (req,res)=>{
+//     res.send("API is running");
+// })
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
+
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+
 app.use(notFound);
 app.use(errorHandler)
 
